f2a4835a417e53fbc0e8ad8095e02a81354a3b3e
CedricPedric <99059069@mydavinci.nl>
2021-09-06 16:35:33 +0200

    python bestanden commit

--- feestlunch.py
+++ feestlunch.py
@@ -4,4 +4,4 @@ kortingbon = 3
 a = (croissant * 0.39) + (stokbrood * 2.78) - (kortingbon * 0.50)
 print(a)
 if a == (18.88):
-    print("De feestlunch kost je bij de bakker" + str(a) + "euro voor de 17 croissantjes en de 2 stokbroden als 3 kortingsbonnen nog geldig zijn!")
+    print("De feestlunch kost je bij de bakker " + str(a) + " euro voor de " + str(croissant) + ' croissantjes en de ' + str(stokbrood) + " stokbroden als " + str(kortingbon) + " kortingsbonnen nog geldig zijn!")
--- speelhaldag.py
+++ speelhaldag.py
@@ -3,4 +3,4 @@ vrbrilmin = 45
 b = (tickets * 7.45) + (vrbrilmin/5 *  0.37)
 print(b)
 if b == (44.44):
-    print('Dit geweldige dagje-uit met'+ str(tickets) + 'mensen in de Speelhal met' + str(vrbrilmin) + 'minuten VR kost je maar' + str(b) + 'euro')
+    print('Dit geweldige dagje-uit met '+ str(tickets) + ' mensen in de Speelhal met ' + str(vrbrilmin) + ' minuten VR kost je maar ' + str(b) + ' euro')
