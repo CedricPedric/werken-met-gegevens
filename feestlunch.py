836a44a2a6de7a1a784feab23d802e768998856a
CedricPedric <99059069@mydavinci.nl>
2021-09-06 15:53:13 +0200

    python bestanden commit

---
+++ feestlunch.py
@@ -0,0 +1,10 @@
+croissant = 0.39 * 17
+stokbrood = 2.78 * 2
+kortingbon = 0.50 * 3
+a = croissant + stokbrood - kortingbon
+print(a)
+
+tickets = 7.45 * 3
+vrbril = 0.37 * 9
+b = tickets + vrbril
+print(b)
---
+++ speelhaldag.py
@@ -0,0 +1,4 @@
+tickets = 7.45 * 3
+vrbril = 0.37 * 9
+b = tickets + vrbril
+print(b)
