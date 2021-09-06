0446e67e97e8e91362c265641783fdcf2772c81c
CedricPedric <99059069@mydavinci.nl>
2021-09-06 16:29:07 +0200

    code met nette output

--- feestlunch.py
+++ feestlunch.py
@@ -1,10 +1,7 @@
-croissant = 0.39 * 17
-stokbrood = 2.78 * 2
-kortingbon = 0.50 * 3
-a = croissant + stokbrood - kortingbon
+croissant = 17
+stokbrood = 2
+kortingbon = 3
+a = (croissant * 0.39) + (stokbrood * 2.78) - (kortingbon * 0.50)
 print(a)
-
-tickets = 7.45 * 3
-vrbril = 0.37 * 9
-b = tickets + vrbril
-print(b)
+if a == (18.88):
+    print("De feestlunch kost je bij de bakker" + str(a) + "euro voor de 17 croissantjes en de 2 stokbroden als 3 kortingsbonnen nog geldig zijn!")
--- speelhaldag.py
+++ speelhaldag.py
@@ -1,4 +1,6 @@
-tickets = 7.45 * 3
-vrbril = 0.37 * 9
-b = tickets + vrbril
+tickets = 3
+vrbrilmin = 45
+b = (tickets * 7.45) + (vrbrilmin/5 *  0.37)
 print(b)
+if b == (44.44):
+    print('Dit geweldige dagje-uit met'+ str(tickets) + 'mensen in de Speelhal met' + str(vrbrilmin) + 'minuten VR kost je maar' + str(b) + 'euro')
